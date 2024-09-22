0e2d8a83579d7e4ec408e3b3f08bb4d04169edff
Raghunandan <raghunandan092@gmail.com>
2024-09-21 20:54:50 -0400

    api versions requests fix

--- app/main.py
+++ app/main.py
@@ -19,8 +19,8 @@ def create_response(api_version, api_key, correlation_id):
     else:
         error_code = struct.pack('>h',0)
 
-    api_key_count = struct.pack('>B', 1)
-    throttle_time_ms = struct.pack('>I', 0)
+    api_key_count = struct.pack('>h', 1)
+    throttle_time_ms = struct.pack('>i', 0)
     api_key_entry = struct.pack('>hhh', api_key, 0, 4)
     body = error_code + api_key_count + api_key_entry + throttle_time_ms
     total_length = 4 + len(header) + len(body)  # 4 bytes for length field itself
@@ -37,7 +37,7 @@ def main():
         print(f"Connection from {client_address}")
 
         try:
-            request_header = client_socket.recv(12)
+            request_header = client_socket.recv(1024)
             print(f"request header: {request_header}")
             if len(request_header) < 12:
                 print("Incomplete header received")
@@ -45,12 +45,9 @@ def main():
             api_key, api_version, correlation_id, client_id = parse_header(request_header)
             print(f"Received header: API Key: {api_key}, Version: {api_version}, Correlation ID: {correlation_id}, Client ID: {client_id}")
 
-            
-            request_body = client_socket.recv(1024)
-            print(f"Request body: {request_body}")
-
             full_response = create_response(api_version, api_key, correlation_id)
             client_socket.sendall(full_response)
+            print(f"Sent response :{full_response.hex()}")
 
         except Exception as e:
             print(f"Error handling client: {e}")
